b532179be9c810d566ea54bbad5bca3584169255
Andreas Schneider <asn@cryptomilk.org>
2017-11-03 08:15:00 +0100

    recovery: Add timezone information

--- platform.mk
+++ platform.mk
@@ -6,3 +6,9 @@ LOCAL_PATH := device/sony/yoshino
 PRODUCT_PACKAGES += \
     fstab.yoshino \
     init.yoshino.pwr
+
+### RECOVERY
+
+# Timezone information
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
