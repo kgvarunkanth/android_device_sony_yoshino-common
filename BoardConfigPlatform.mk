72b213d1af822ed310789d4a367cd8e77bc7369e
Andreas Schneider <asn@cryptomilk.org>
2017-11-18 10:40:29 +0100

    HACK selinux permissive

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -56,6 +56,10 @@ BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += zram.backend=z3fold
 
+# !!! TEMPORARY HACK
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+# !!! TEMPORARY HACK
+
 # See README_Xperia in Kernel Source
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
